6ed0b56689dcfc71e1564d89a1553f187a29a695
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-05-06 22:33:09 -0400

    sada

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import { DOMSelectors } from "./DOM";
+
 const key = "RGAPI-360739c5-1a96-46f6-b942-bcf95baa916c";
 
 const query = async function () {
@@ -5,8 +7,40 @@ const query = async function () {
     const response = await fetch(
       `http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json?api_key=${key}`
     );
-    const data = await response.json();
-    console.log(data);
+    const info = await response.json();
+    console.log(info.data);
+    const champData = info.data;
+
+    Object.values(champData).forEach((champion) => {
+      console.log(champion);
+
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="movie-card">
+        <div class="movie-card-front">
+          <img
+            src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg"
+            alt=""
+            class="poster"
+          />
+        </div>
+        <div class="movie-card-back">
+          <h3 class="movie-card-header">${champion.id}</h3>
+          <div class="score-box">
+
+            <p class="user-score">${champion.title}</p>
+          </div>
+          <div class="release-box">
+            <p class="release-date">about</p>
+            <p class="release-date">${champion.blurb}</p>
+          </div>
+          <div class="movie-genres">
+Click to learn more
+          </div>
+        </div>
+      </div> `
+      );
+    });
   } catch (error) {
     console.log(error);
     alert("jp");
