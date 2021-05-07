91be75baa74c8079e5558490c87afa740128f437
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-05-06 23:15:47 -0400

    zx

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const key = "RGAPI-360739c5-1a96-46f6-b942-bcf95baa916c";
 const query = async function () {
   try {
     const response = await fetch(
-      `http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json?api_key=${key}`
+      `https://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json?api_key=${key}`
     );
     const info = await response.json();
     console.log(info.data);
