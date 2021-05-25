f8466fce9e2a0bab128ca4e4550386fffdb07b93
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-05-25 00:34:57 -0400

    asdas

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,7 @@
 const DOMSelectors = {
   grid: document.querySelector(".movie-grid"),
+  searchForm: document.querySelector("#submit-btn"),
+  searchArea: document.querySelector(".srch-bar"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,37 @@ const key = "RGAPI-360739c5-1a96-46f6-b942-bcf95baa916c";
 //return new Promise((resolve, reject) => {});
 //}
 //return data
+
+function dspChamps(cData) {
+  Object.values(cData).forEach((champion) => {
+    DOMSelectors.grid.insertAdjacentHTML(
+      "beforeend",
+      ` <div class="champ-card">
+      <div class="champ-card-front">
+        <img
+          src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="champ-card-back">
+        <h3 class="champ-card-header">${champion.id}</h3>
+        <div class="title-box">
+          <p class="title">${champion.title}</p>
+        </div>
+        <div class="about-box">
+          <p class="ab">about</p>
+          <p class="ab">${champion.blurb}</p>
+        </div>
+        <div class="lrn-more">
+Click to learn more
+        </div>
+      </div>
+    </div> `
+    );
+  });
+}
+
 const query = async function () {
   try {
     const response = await fetch(
@@ -19,39 +50,22 @@ const query = async function () {
     console.log(info.data);
     const champData = info.data;
 
-    Object.values(champData).forEach((champion) => {
-      console.log(champion);
-
-      DOMSelectors.grid.insertAdjacentHTML(
-        "beforeend",
-        ` <div class="movie-card">
-        <div class="movie-card-front">
-          <img
-            src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg"
-            alt=""
-            class="poster"
-          />
-        </div>
-        <div class="movie-card-back">
-          <h3 class="movie-card-header">${champion.id}</h3>
-          <div class="score-box">
-
-            <p class="user-score">${champion.title}</p>
-          </div>
-          <div class="release-box">
-            <p class="release-date">about</p>
-            <p class="release-date">${champion.blurb}</p>
-          </div>
-          <div class="movie-genres">
-Click to learn more
-          </div>
-        </div>
-      </div> `
-      );
-    });
+    dspChamps(champData);
   } catch (error) {
     console.log(error);
     alert("jp");
   }
 };
 query();
+
+const listen = function () {
+  console.log("uwu");
+  DOMSelectors.searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const searchParams = DOMSelectors.searchArea.value;
+    console.log(searchParams);
+    //const searchQuery = async function () {};
+  });
+};
+
+listen();
