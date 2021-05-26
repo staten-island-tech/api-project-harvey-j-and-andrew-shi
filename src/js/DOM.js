9a00cafa5b62fcc1db9755fa9433f4854a4f4f73
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2021-05-26 00:00:16 -0400

    zxc

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -2,6 +2,8 @@ const DOMSelectors = {
   grid: document.querySelector(".movie-grid"),
   searchForm: document.querySelector("#submit-btn"),
   searchArea: document.querySelector(".srch-bar"),
+  lrnMore: document.querySelector(".champ-card-back-btn"),
+  homeButton: document.querySelector(".home-btn"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,14 @@
 import { DOMSelectors } from "./DOM";
 
 const key = "RGAPI-360739c5-1a96-46f6-b942-bcf95baa916c";
-//  DOMselectors.startBtn.addEventListener("click", () => {
-//DOMselectors.displayContainer.classList.remove("d-none");
-//DOMselectors.startPage.classList.add("d-none");
-//displayQues();
-//});
-//function fetch(key) {
-//return new Promise((resolve, reject) => {});
-//}
-//return data
 
-function dspChamps(cData) {
-  Object.values(cData).forEach((champion) => {
-    DOMSelectors.grid.insertAdjacentHTML(
-      "beforeend",
-      ` <div class="champ-card">
+const dspChamps = async function (cData) {
+  try {
+    DOMSelectors.grid.innerHTML = "";
+    Object.values(cData).forEach((champion) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `  <div class="champ-card">
       <div class="champ-card-front">
         <img
           src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg"
@@ -37,9 +30,13 @@ Click to learn more
         </div>
       </div>
     </div> `
-    );
-  });
-}
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("err dsp champ not working");
+  }
+};
 
 const query = async function () {
   try {
@@ -47,25 +44,46 @@ const query = async function () {
       `http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json?api_key=${key}`
     );
     const info = await response.json();
-    console.log(info.data);
-    const champData = info.data;
-
-    dspChamps(champData);
+    dspChamps(info.data);
   } catch (error) {
     console.log(error);
-    alert("jp");
+    alert("err api fetch does not work");
   }
 };
-query();
 
 const listen = function () {
-  console.log("uwu");
   DOMSelectors.searchForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const searchParams = DOMSelectors.searchArea.value;
-    console.log(searchParams);
-    //const searchQuery = async function () {};
+    const searchQuery = async function () {
+      try {
+        const champResponse = await fetch(
+          `http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion/${searchParams}.json?api_key=${key}`
+        );
+        const champInfo = await champResponse.json();
+        DOMSelectors.grid.innerHTML = "";
+        dspChamps(champInfo.data);
+      } catch (error) {
+        console.log(error);
+        alert("err invaild champion");
+      }
+    };
+    searchQuery();
   });
 };
 
+const home = async function () {
+  try {
+    DOMSelectors.homeButton.addEventListener("click", () => {
+      DOMSelectors.grid.innerHTML = "";
+      query();
+    });
+  } catch (error) {
+    console.log(error);
+    alert("err home btn not working");
+  }
+};
+
+query();
+home();
 listen();
